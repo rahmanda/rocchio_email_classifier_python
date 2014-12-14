ec517e3b601002a3133f050edce5aa6f88746365
rahmandawibowo <rahmandawibowo@gmail.com>
2014-12-14 22:46:49 +0700

    add term weight model class

---
+++ TermWeight.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import MySQLdb as mdb
+import json
+import DBConnect
+import math
+
+class TermWeight:
+
+	_table = 'TermWeight'
+	_con = null
+
+	def __init__(self):
+		self._con = DBConnect()
+
+	# insert tfidf calculation of a term in a document into term weight table
+	# return terms weight array
+	def add_terms_weight(self):
+		terms_weight = {}
+		con = self._con
+		table = self._table
+		cur = con.cursor()
+
+		cur.execute("SELECT * FROM emails") 
+		email_count = cur.rowcount
+
+		dicts = get_dicts(con) #what is this
+
+		cur.execute("SELECT * FROM tokens");
+		tokens = cur.fetchall()
+
+		for token in tokens:
+			tfidf = self._calc_tfidf(email_count, token['tf'], dicts[token['term']])
+			cur.execute("INSERT INTO %s VALUES (%d, %s, %d)" % table, token['email_id'], token['term'], tfidf)
+
+	def _calc_tfidf(email_count, tf, df):
+		return tf * math.log(email_count / df)
+
+	def fetch_all(self):
+		# TODO
